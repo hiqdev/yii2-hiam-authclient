f5c940c7096a0de695efae88b1aab09eec2023b0
Andrii Vasyliev <sol@hiqdev.com>
2015-04-17 10:57:30 +0000

    + getActiveClient

--- Collection.php
+++ Collection.php
@@ -9,10 +9,26 @@ namespace hi3a\authclient;
 
 class Collection extends \yii\authclient\Collection
 {
-    public function getClient ($id = null) {
+    /**
+     * Gets first client by default
+     */
+    public function getClient ($id = null)
+    {
         if ($id === null) {
             list($id,$dummy) = each($this->getClients());
         };
         return parent::getClient($id);
+    }
+
+    /**
+     * Finds first active (connected) client
+     */
+    public function getActiveClient ()
+    {
+        foreach ($this->getClients() as $id => $client) {
+            $token = $client->getAccessToken();
+            if ($token) return $client;
+        };
+        return null;
     }
 }
