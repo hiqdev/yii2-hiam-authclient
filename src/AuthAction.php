66e43c2cf5622fc2991f94ebf778ad56837252e5
Andrii Vasyliev <sol@hiqdev.com>
2019-07-31 14:47:15 +0000

    Added `AuthAction` fixing problem with repeated return from HIAM

---
+++ src/AuthAction.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * OAuth2 client for yii2 to login with HIAM server
+ *
+ * @link      https://github.com/hiqdev/yii2-hiam-authclient
+ * @package   yii2-hiam-authclient
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiam\authclient;
+
+use Yii;
+
+class AuthAction extends \yii\authclient\AuthAction
+{
+    public function run()
+    {
+        /// Fixed problem with repeated return from HIAM
+        try {
+            return parent::run();
+        } catch (\Exception $e) {
+            $isLoggedIn = !empty(Yii::$app->user->id);
+            if ($e->getMessage() === 'Invalid auth state parameter.' && $isLoggedIn) {
+                return $this->redirectSuccess();
+            }
+        }
+    }
+}
