b343f16473c215209e16caf078bdc991a4386bff
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2021-05-12 15:03:59 +0300

    Add external user impersonation request support

--- src/AuthAction.php
+++ src/AuthAction.php
@@ -11,26 +11,48 @@
 
 namespace hiam\authclient;
 
+use Exception;
+use Psr\Log\LoggerInterface;
 use Yii;
 use yii\helpers\Html;
 
 class AuthAction extends \yii\authclient\AuthAction
 {
+    private LoggerInterface $log;
+
+    public function __construct($id, $controller, LoggerInterface $log, $config = [])
+    {
+        parent::__construct($id, $controller, $config);
+        $this->log = $log;
+    }
+
     public function run()
     {
         try {
             return parent::run();
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             if ($e->getMessage() !== 'Invalid auth state parameter.') {
                 Yii::$app->getResponse()->setStatusCode(500);
                 return Html::encode($e->getMessage());
             }
 
-            if (!empty(Yii::$app->user->id)) {
-                return $this->redirectSuccess();
-            }
-
-            return '';
+            return $this->handleInvalidAuthState($e);
         }
+    }
+
+    private function handleInvalidAuthState(Exception $exception)
+    {
+        if (!empty(Yii::$app->user->id)) {
+            return $this->redirectSuccess();
+        }
+
+        $errorMessage = sprintf('Failed to exchange the auth code to the access token: %s', $exception->getMessage());
+        $this->log->error($errorMessage, ['exception' => $exception]);
+
+        if (YII_ENV !== 'prod') {
+            echo $errorMessage;
+        }
+
+        return '';
     }
 }
